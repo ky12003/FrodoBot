1d424acad4495a9a00d94cbef556d44e54c0605a
Kenta Yamamoto <kenta12003@gmail.com>
2023-03-10 11:55:47 -0800

    Fixed a bug with inertial and modified skills routine
    -Fixed the left turn with the inertial function
    -Adjusted values for the Skills Autonomous Routine, and added inertial turn instead of manual turn for accuracy

--- src/routines.cpp
+++ src/routines.cpp
@@ -62,21 +62,21 @@ void Routine1Skills() {
   moveForward(-10, 10, 1000);
   wait(300, msec);
   IntakeSpitAuto(340, 70, 5000); // roll the roller
-  moveForward(10, 10, 5000);     // move forward away from the roller
+  moveForward(13, 10, 5000);     // move forward away from the roller
   wait(300, msec);
-  TurninPlace(85, 15, 4000); // turn clockwise towards the basket
+  InertialTurn('r', 30, 87, 4000); // Turn clockwise (right) 90 degrees towards the goal
   wait(300, msec);
   moveForward(165, 20, 3000); // go towards the basket to shoot
   wait(300, msec);
-  TurninPlace(-18, 30, 3000); // turn to face basket
+  InertialTurn('l', 30, 18, 4000); // turn the bot slightly to the left to face the basket
   wait(300, msec);
-  windCatapultAuton();
+  windCatapultAuton(); // wind the catapult up
   wait(300, msec);
   ShootCatapultAuto(4000);    // shoot the preload disks into high goal
   wait(300, msec);
   windCatapultAuton();
   wait(300, msec);
-  TurninPlace(18, 30, 3000); // turn back to original position
+  InertialTurn('r', 30, 18, 4000); // turn back to original position
   wait(300, msec);
 
 
@@ -85,44 +85,46 @@ void Routine1Skills() {
     wait(300, msec);
     moveForward(-55, 20, 4000); // move backwards towards the loading zone
     wait(300, msec);
-    TurninPlace(88, 15, 4000); // turn clockwise for additional preloads
+    InertialTurn('r', 30, 87, 4000); // turn clockwise towards the loading zone
     wait(300, msec);
     moveForward(30 /*+ (i * 3.5)*/, 10, 5000); // move forwards towards loading zone
     wait(3000, msec);
     moveForward(-5, 20, 3000); // move backwards away from the loading zone
     wait(300, msec);
-    TurninPlace(-90, 15, 4000); // turn counterclockwise towards goal
+    InertialTurn('l', 30, 87, 4000); // turn counterclockwise towards goal
     wait(300, msec);
     moveForward(53, 15, 4000); // move forwards towards goal
     wait(300, msec);
-    TurninPlace(-25, 30, 3000); // turn to face basket
+    InertialTurn('l', 30, 25, 4000); // turn counterclockwise to face basket in order to shoot
     wait(300, msec);
-    windCatapultAuton();
+    windCatapultAuton(); // wind the catapult up
     wait(300, msec);
     ShootCatapultAuto(4000);    // shoot the preload disks into high goal
     wait(200, msec);
     windCatapultAuton();
     wait(300, msec);
-    TurninPlace(24, 30, 3000); // turn back to original position
+    InertialTurn('r', 30, 25, 4000); // turn back to original position
   }
 
   wait(300, msec);
-  TurninPlace(-5, 30, 2000);
-  wait(300, msec);
   moveForward(-175, 40, 6000); // move backwards to other roller
   wait(300, msec);
-  TurninPlace(90, 15, 4000); // turn clockwise to adjust to the roller position
+  InertialTurn('r', 30, 87, 4000);  // turn clockwise to adjust to the roller position
   wait(300, msec);
   moveForward(-5, 35, 6000); // move backwards to adjust to the roller position
   wait(300, msec);
-  TurninPlace(-90, 15, 4000); // turn counterclockwise to adjust to the roller position
+  InertialTurn('l', 30, 87, 4000);  // turn counterclockwise to adjust to the roller position
   wait(300, msec);
   moveForward(-52, 15, 6000); // move backwards torwards the roller
   wait(300, msec);
   IntakeSpitAuto(400, 70, 5000); // roll the roller
   wait(300, msec);
   moveForward(40, 40, 3000); // move forward for expansion
-  TurninPlace(-70, 50, 3000); // turn to get the tiles
+  wait(300, msec);
+  InertialTurn('l', 30, 30, 4000); // turn counterclockwise to get ready for expansion
+  wait(300, msec);
+  moveForward(-20, 40, 3000); // move slightly backwards for more tiles
+  wait(300, msec);
   ExpansionLeft.set(1); // shoot expansion
   // wait(200, msec);
   // moveForward(25, 60, 2000); // move forward slightly to get more tiles
@@ -171,7 +173,7 @@ TEST AUTONS
 --*/
 void testAuton() {
   // printf("test");
-  InertialTurn('r', 40, 90, 6000);
+  InertialTurn('l', 40, 90, 6000);
 }
 
 void GPStest() { DriveToTargetXY(1, 1, 5, 10); }
